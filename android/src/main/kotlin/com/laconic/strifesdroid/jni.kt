c50ccdf73c73229de84402c00321bb4f843f2992
Matthew Kersey <matt@scorebooklive.com>
2024-12-31 20:47:11 -0500

    hide hyperxengine module

--- android/src/main/kotlin/com/laconic/strifesdroid/jni.kt
+++
@@ -1,6 +0,0 @@
-package com.laconic.strifesdroid
-
-external fun startAudioEngine(cpuIds: IntArray)
-external fun stopAudioEngine()
-external fun pauseAudioEngine()
-external fun resumeAudioEngine()
